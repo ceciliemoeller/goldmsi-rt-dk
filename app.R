412351e4f2c1c532279efb5c511a67915c761b69
Peter M C Harrison <pmc.harrison@gmail.com>
2019-09-25 16:32:36 +0100

    Prototype jsPsych wrapper in psychTestR

---
+++ app.R
@@ -0,0 +1,56 @@
+library(htmltools)
+library(psychTestR)
+
+library_dir <- "jspsych/jspsych-6.1.0"
+custom_dir <- "jspsych/js"
+
+head <- tags$head(
+  # jsPsych library files
+  includeScript(file.path(library_dir, "jspsych.js")),
+  includeScript(file.path(library_dir, "plugins/jspsych-html-keyboard-response.js")),
+  includeScript(file.path(library_dir, "plugins/jspsych-image-keyboard-response.js")),
+  includeScript(file.path(library_dir, "plugins/jspsych-audio-keyboard-response.js")),
+  # Custom files
+  includeScript(file.path(custom_dir, "welcome.js")),
+  includeScript(file.path(custom_dir,"sv_training.js")),
+  includeScript(file.path(custom_dir,"sv_real.js")),
+  includeScript(file.path(custom_dir,"cv_training.js")),
+  includeScript(file.path(custom_dir,"cv_real.js")),
+  includeScript(file.path(custom_dir,"sa_training.js")),
+  includeScript(file.path(custom_dir,"sa_real.js")),
+  includeCSS(file.path(library_dir, "css/jspsych.css")),
+  includeCSS("jspsych/css/RT_DK.css")
+)
+
+ui <- tags$div(
+  head,
+  includeScript("jspsych/run_jspsych.js"),
+  tags$div(id = "js_psych", style = "min-height: 100vh")
+)
+
+elt_jspsych <- page(
+  ui = ui,
+  label = "jspsych",
+  get_answer = function(input, ...) input$jspsych_results,
+  validate = function(answer, ...) nchar(answer) > 0L,
+  save_answer = TRUE
+)
+
+if (FALSE) {
+  # To launch the test manually, run the following command.
+  # Note that other ways of launching the app may fail to provide access to
+  # the required media files.
+  shiny::runApp(".")
+}
+
+make_test(
+  elt = list(
+    elt_jspsych,
+    final_page("You finished the test.")
+  ), 
+  opt = demo_options(display = display_options(
+    full_screen = TRUE,
+    content_background_colour = "grey",
+    css = c(file.path(library_dir, "css/jspsych.css"), 
+            "jspsych/css/RT_DK.css")
+  )))
---
+++ jspsych/run_jspsych.js
@@ -0,0 +1,59 @@
+// https://www.jspsych.org/tutorials/rt-task/#part-1-creating-a-blank-experiment
+/* define sub experiments. Her skal jeg hente koden ind fra de 3 filer*/
+var sub_experiments = [timeline_sv, timeline_cv, timeline_sa];
+
+var shuffledArray = jsPsych.randomization.repeat(sub_experiments, 1);
+
+// timeline = timeline.concat(shuffledArray)
+
+var arrayLength = shuffledArray.length;
+for (var i = 0; i < arrayLength; i++) {
+    //console.dir(shuffledArray[i]);
+    for (var key in shuffledArray[i]) {
+      if (shuffledArray[i].hasOwnProperty(key)) {
+        console.log(key + " -> " + shuffledArray[i][key]);
+        console.dir(shuffledArray[i][key]);
+        timeline.push(shuffledArray[i][key]);
+      }
+    }
+}
+
+// timeline.push(shuffledArray)
+
+
+// var rand_procedure = {
+//   timeline: timeline
+//   timeline_variables: sub_experiments,
+//   randomize_order: true
+// }
+
+// var training_procedure = {
+//     timeline: [training_fixation, training],
+//     timeline_variables: training_stimuli,
+//     randomize_order: true,
+//     repetitions: 2
+
+// }
+
+// timeline.push(instructions);
+//   timeline.push(test_procedure);
+//   timeline.push(debrief_block);
+
+var images = ['img/cross.png','img/blankbox.png','img/cRT_fix.png', 'img/cRT1.png', 'img/cRT2.png', 'img/cRT3.png', 'img/cRT4.png'];
+var audio = ['sounds/452sine.mp3'];
+
+function run_jspsych() {
+  jsPsych.init({
+    timeline: timeline,
+    display_element: 'js_psych',
+    preload_images: images,
+    preload_audio: audio,
+    use_webaudio: true,
+    on_finish: function() {
+      var json_data = jsPsych.data.get().json();
+      Shiny.onInputChange("jspsych_results", json_data);
+      next_page();
+    }
+  });
+}
+run_jspsych();
