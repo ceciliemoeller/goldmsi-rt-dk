474bc4c9838a8bf61227b5c4dcfaf99e6538a902
Cecilie Møller <au213911@uni.au.dk>
2019-12-19 10:55:33 +0100

    Final launced version 19.12.2019

--- app.R
+++ app.R
@@ -68,8 +68,8 @@ ui <- tags$div(
 
 # Configure options
 config <- test_options(title="Validering af dansk Gold-MSI",
-                       # admin_password="", # write a secret password here
-                       enable_admin_panel=FALSE,
+                       admin_password="", # write a secret password here
+                       # enable_admin_panel=FALSE,
                        researcher_email="cecilie@clin.au.dk",
                        problems_info="Problemer? Kontakt venligst Cecilie Møller på cecilie@clin.au.dk",
                        languages = "DA",
